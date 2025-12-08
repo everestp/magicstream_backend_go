071ccd47e6050914d9c7c641f9e1a5016f1dd784
everestp <paudeleverest09@gmail.com>
2025-12-09 01:06:42 +0545

    Feat :Schema and database setup

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
+	router := gin.Default()
+	router.GET("/hello", func (c *gin.Context)  {
+		c.String(200,  "Hello magic stream")
+		
+	})
+
+	 if err := router.Run(":8080"); err != nil{
+		fmt.Println("Failed to start server",err)
+	 }
 	fmt.Println("This is the first  go  code")
 }
